7094f52e26d21da92d505f6b0f1ec8d5ad425f06
Ranveer225 <singr12@farmingdale.edu>
2024-09-23 19:21:51 -0400

    Create Robot class

---
+++ src/main/java/org/example/groupproject1_csc311/Robot.java
@@ -0,0 +1,25 @@
+package org.example.groupproject1_csc311;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class Robot {
+    private double x;
+    private double y;
+    private Image robotImage;
+
+    public Robot(double x, double y) {
+        this.x = x;
+        this.y = y;
+        this.robotImage = new Image("path/to/robot.png");
+    }
+
+    public void move(double dx, double dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public void draw(GraphicsContext gc) {
+        gc.drawImage(robotImage, x, y);
+    }
+}
