150faf367dcff372bb98b2251e01426626791912
Ranveer225 <singr12@farmingdale.edu>
2024-09-23 19:55:42 -0400

    Create Robot class

--- src/main/java/org/example/groupproject1_csc311/HelloApplication.java
+++ src/main/java/org/example/groupproject1_csc311/HelloApplication.java
@@ -1,26 +1,56 @@
 package org.example.groupproject1_csc311;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
 /**
  * Title: Group Project 1
  * Description:
  * @author Cody Bandrowski, Other Authors
  */
 public class HelloApplication extends Application {
+    private Robot robot;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Group Project 1  ");
+    public void start(Stage stage) {
+
+        Canvas canvas = new Canvas(600, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+
+        Image mazeImage = new Image("path/to/maze.png");
+        gc.drawImage(mazeImage, 0, 0);
+
+
+        robot = new Robot(50, 50);
+        robot.draw(gc);
+
+
+        StackPane root = new StackPane();
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root, 600, 600);
+
+
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case UP -> robot.move(0, -5);
+                case DOWN -> robot.move(0, 5);
+                case LEFT -> robot.move(-5, 0);
+                case RIGHT -> robot.move(5, 0);
+            }
+
+
+            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            gc.drawImage(mazeImage, 0, 0);
+            robot.draw(gc);  // Redraw robot
+        });
+
+        stage.setTitle("Maze Game");
         stage.setScene(scene);
-        Group grp= new Group();
         stage.show();
     }
 
--- src/main/java/org/example/groupproject1_csc311/Robot.java
+++ src/main/java/org/example/groupproject1_csc311/Robot.java
@@ -23,3 +23,4 @@ public class Robot {
         gc.drawImage(robotImage, x, y);
     }
 }
+
